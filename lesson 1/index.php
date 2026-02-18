9118a6aa81e7f3c19aa9953c25d42b51e037a2d7
Isaque Menezes <isaquefreitasmenezes@gmail.com>
2026-02-17 23:17:18 -0300

    first lesson

---
+++ lesson 1/index.php
@@ -0,0 +1,19 @@
+<?php 
+    echo"I love pizza <br>";
+    echo"it's pretty good";
+    // This is a comment
+    /* This is a multiline comment */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
