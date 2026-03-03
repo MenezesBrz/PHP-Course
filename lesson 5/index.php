15285ebeb26a6df388c5b00fca9bc8b4311e5f46
Isaque Menezes <isaquefreitasmenezes@gmail.com>
2026-03-02 21:03:12 -0300

    lesson 5

---
+++ lesson 5/index.php
@@ -0,0 +1,35 @@
+<?php 
+    $age = 110;
+
+    if ($age >= 100){
+        echo"You are too old to enter this site <br>";
+    } else if ($age >= 18) {
+        echo"You may enter this site! <br>";
+    } else if($age <= 0){
+     echo"That wasn't a valid age <br>";   
+    } else {
+        echo"You must be 18+ to enter! <br>";
+    }
+
+    $adult = true;
+
+    if($adult == true){
+        echo"You may enter this site <br>";
+    } else {
+        echo"You must be an adult to enter <br>";
+    }
+
+    $hours = 50;
+    $rate = 15;
+    $weekly_pay = null;
+
+    if($hours <= 0){
+        $weekly_pay = 0;
+    } else if ($hours <= 40) {
+        $weekly_pay = $hours * $rate;
+    } else {
+        $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
+    }
+
+    echo"You made \${$weekly_pay} this week";
+?>
